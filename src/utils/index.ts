1ecaad87429c392a3f2ffe202ef27d5edcd34b21
Jędrek Dąbrowski <jedrzej.dabrowski@aexol.com>
2022-11-09 14:13:10 +0100

    add .env file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,17 +2,21 @@ import Mailgun from 'mailgun.js';
 import formData from 'form-data';
 import { GraphQLTypes } from '../zeus';
 
-const getMailgunEnvs = () => {
+const { key, domain, from } = (() => {
   const { MAILGUN_API_KEY, MAILGUN_DOMAIN, MAILGUN_SENDER } = process.env;
-  if (!MAILGUN_API_KEY || !MAILGUN_DOMAIN || MAILGUN_SENDER) {
+  if (!MAILGUN_API_KEY || !MAILGUN_DOMAIN || !MAILGUN_SENDER) {
     throw new Error('mailgun envs not set');
   }
-  return { apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN, from: MAILGUN_SENDER };
-};
+  return { key: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN, from: MAILGUN_SENDER };
+})();
 
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({ key: getMailgunEnvs().apiKey, url: 'https://api.eu.mailgun.net', username: 'gei-mailgun' });
+const mg = mailgun.client({ key, url: `https://api.mailgun.net/`, username: 'gei-mailgun' });
 
-export const sendMessage = ({ message, to, subject }: GraphQLTypes['MailgunData']) => {
-  return mg.messages.create(getMailgunEnvs().domain, { from: getMailgunEnvs().from, to, subject, text: message });
-};
+export const sendMessage = ({ message, to, subject }: GraphQLTypes['MailgunData']) =>
+  mg.messages.create(domain, {
+    from,
+    to,
+    subject,
+    text: message,
+  });
